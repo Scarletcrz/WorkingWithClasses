78a5261ca506a2c7840d7867479d1cbb6e3069e5
Scarlet Cruz <scruz@appdev.yearup.org>
2023-04-18 15:43:44 -0400

    created a constructor and add a name getter

--- src/main/java/com/sc/WorkingWithClasses.java
+++ src/main/java/com/sc/WorkingWithClasses.java
@@ -2,6 +2,41 @@ package com.sc;
 
 public class WorkingWithClasses {
      public static void main(String[] arg){
+          Person scarlet = new Person("Scarlet", 29, "06-24-1993", true,"000-00-0000);
+//          scarlet.name = "Scarlet";
+//          scarlet.age = 29;
+//          scarlet.birthday = "06-24-1993";
+//          scarlet.hasTattoo = true;
+//          System.out.println(scarlet.name);
+
+          System.out.println();
+     }
+}
+
+class Person {
+     //This is the mold to the code.(Cookie cutter)
+     // Properties
+    private String name;
+    private int age;
+    private String birthday;
+    private boolean hasTattoo;
+    private  String ssn =
+
+     // Constructor method
+     public Person (String name, int age, String birthday, boolean hsTattoo, String ssn){
+          this.name = name;
+          this.age = age;
+          this.birthday = birthday;
+          this.hasTattoo = hasTattoo;
+          this.ssn = ssn;
 
      }
+
+     // Methods- Getter and Setters
+     public String getName(){
+         return this.name;
+     }
+
+
+
 }
